ad0b906b296411bd1141ea22be48bd221fa0614a
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2024-10-17 21:47:09 +0900

    first commit

---
+++ src/bin/ort-mnist.rs
@@ -0,0 +1,34 @@
+use std::time::Instant;
+
+fn main() -> anyhow::Result<()> {
+    let input_image: Vec<f32> = compare_onnx_runtimes::mnist_input_image();
+    let input_tensor =
+        ort::Value::from_array(([1usize, 1, 28, 28], input_image.into_boxed_slice()))?;
+
+    ort::init().with_name("mnist").commit()?;
+
+    let session = ort::Session::builder()?
+        .with_optimization_level(ort::GraphOptimizationLevel::Level1)?
+        .with_intra_threads(4)?
+        .commit_from_file("models/mnist.onnx")?;
+
+    let start = Instant::now();
+    let outputs = session.run(ort::inputs![input_tensor]?)?;
+    let end = start.elapsed();
+
+    let tokens = outputs["Plus214_Output_0"].try_extract_tensor::<f32>()?;
+    println!("{}", tokens);
+    let mut argmax = f32::MIN;
+    let mut ans = 0;
+    for (i, x) in tokens.into_iter().enumerate() {
+        println!("{}: {}", i, x);
+        if *x > argmax {
+            argmax = *x;
+            ans = i;
+        }
+    }
+    println!("answer is {}", ans);
+    println!("time: {}.{:09}", end.as_secs(), end.subsec_nanos());
+
+    Ok(())
+}
